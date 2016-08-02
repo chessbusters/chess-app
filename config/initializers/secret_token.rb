00fe8943021fa82dda6f095fdbf077b53111740e
Antoine <maciaantoine@yahoo.fr>
2016-08-02 03:43:22 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chessbuster::Application.config.secret_key_base = '2e40d1b101f61d3bee3197259b2edce5ce64eee6ddd84ef24f1a07f4eba804b749b352562eeb46553212737f8ec57a14b752fdb7c056c6afc11017ff7fd3646c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chessbuster::Application.config.session_store :cookie_store, key: '_chessbuster_session'
