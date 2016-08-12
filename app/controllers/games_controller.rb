e61dbc60ed5396e9dc5a8285e1c0b0b023905861
Antoine <maciaantoine@yahoo.fr>
2016-08-12 11:22:59 +0000

    setup views for testing and logic for open rooms accessible from index page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,9 @@
 # Controller for app CRUD logic
 class GamesController < ApplicationController
+before_action :authenticate_user!
+
   def index
+    @game = Game.all
   end
 
   def new
@@ -8,21 +11,31 @@ class GamesController < ApplicationController
   end
 
   def create
-    @game = current_user.white_games.create(game_params)
+    @game = Game.create(:white_user_id => current_user[:id] )
     if @game.valid?
-      redirect_to root_path
+      redirect_to game_path(@game)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
-    @game = Game.find(params[:id])
+     @game = Game.find(params[:id])
+    if @game.white_user_id == current_user[:id]
+      redirect_to root_path, alert: "Need another oppenent than creator :O)"
+      elsif @game.white_user_id != nil && @game.black_user_id == nil
+      @game.black_user_id = current_user[:id]
+    end
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:name)
+    params.require(:game).permit(:white_user_id)
+  end
+
+  helper_method :current_game
+  def current_game
+    @current_game ||= Game.find(params[:id])
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,6 @@
 class Game < ActiveRecord::Base
   belongs_to :white_user, class_name: 'User', foreign_key: 'white_user_id'
   has_many :pieces
+  belongs_to :user
   delegate :bishops, :rooks, :pawns, :kings, :queens, :knights, to: :pieces
 end
