641b60d89b10609766e30fc81648310dbc03da1f
Joe Spedale <jspedale@gmail.com>
2016-08-12 03:09:22 +0000

    Added (WIP) populate_board! method and rspec test. Not currently functioning.

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,40 +4,48 @@ class Game < ActiveRecord::Base
   has_many :pieces
   delegate :bishops, :rooks, :pawns, :kings, :queens, :knights, to: :pieces
 
+  after_create :populate_board!
+
   def populate_board!
-    King.create(game: self, color:  x_coordinate:, y_coordinate: )
-
+    King.create(game: self, color: 'white', x_coordinate: 3, y_coordinate: 0)
+    King.create(game: self, color: 'black', x_coordinate: 3, y_coordinate: 7)
+    Queen.create(game: self, color: 'white', x_coordinate: 4, y_coordinate: 0)
+    Queen.create(game: self, color: 'black', x_coordinate: 4, y_coordinate: 7)
+    Bishop.create(game: self, color: 'white', x_coordinate: 2, y_coordinate: 0)
+    Bishop.create(game: self, color: 'white', x_coordinate: 5, y_coordinate: 0)
+    Bishop.create(game: self, color: 'black', x_coordinate: 2, y_coordinate: 7)
+    Bishop.create(game: self, color: 'black', x_coordinate: 5, y_coordinate: 7)
+    Knight.create(game: self, color: 'white', x_coordinate: 1, y_coordinate: 0)
+    Knight.create(game: self, color: 'white', x_coordinate: 6, y_coordinate: 0)
+    Knight.create(game: self, color: 'black', x_coordinate: 1, y_coordinate: 7)
+    Knight.create(game: self, color: 'black', x_coordinate: 6, y_coordinate: 7)
+    Rook.create(game: self, color: 'white', x_coordinate: 0, y_coordinate: 0)
+    Rook.create(game: self, color: 'white', x_coordinate: 7, y_coordinate: 0)
+    Rook.create(game: self, color: 'black', x_coordinate: 0, y_coordinate: 7)
+    Rook.create(game: self, color: 'black', x_coordinate: 7, y_coordinate: 7)
+    create_white_pawn
+    create_black_pawn
   end
 
-  def create_piece
-    Pawn.create.....
-
-  end
-
-
-  def white_pawn
+  def create_white_pawn
     (0..7).each do |x|
       Pawn.create(
-      game_id = id;
-      x_coordinate = x;
-      y_coordinate = 1;)
+        game: self,
+        color: 'white',
+        x_coordinate: x,
+        y_coordinate: 1
+      )
     end
   end
 
-  def black_pawn
+  def create_black_pawn
     (0..7).each do |x|
       Pawn.create(
-      game_id = id;
-      x_coordinate = x;
-      y_coordinate = 6;)
+        game: self,
+        color: 'white',
+        x_coordinate: x,
+        y_coordinate: 6
+      )
     end
   end
-
-# YOU NEED TO DO A MIGRATION FOR PIECE COLOR 
-
-
-
-
-
-
 end
---
+++ spec/models/game_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Game, type: :model do
+    describe 'populate board' do
+      it 'should populate the board with 32 pieces' do
+        Game.create
+        expect(game.pieces.count).to eq 32
+      end
+    end
+end
