8801e816d648420e2392023c73eb81c3d75c6de4
Antoine <maciaantoine@yahoo.fr>
2016-08-23 02:18:54 +0000

    polished pawn valid_move?, made all tests pass

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -4,39 +4,32 @@ class Pawn < Piece
     !obstructed?(future_x, future_y) && valid_pawn_range?(future_x, future_y)
   end
 
-  # Returns true if the king is only moving from one block
+  # Returns true if the pawn is only moving from one block
   def valid_pawn_range?(future_x, future_y)
     x_diff, y_diff = find_diffs(future_x, future_y)
-    return true if move_front?(x_diff, y_diff)
+    return true if move_forward?(x_diff, y_diff)
     false
   end
 
   private
 
   def find_diffs(future_x, future_y)
-    if (future_y > y_coordinate) && color == 'black'
-      x_diff = (x_coordinate - future_x).abs
-      y_diff = (y_coordinate - 0).abs
-    elsif (future_y < y_coordinate) && color == 'white'
-      x_diff = (x_coordinate - future_x).abs
-      y_diff = (y_coordinate - 0).abs
-    else
-      x_diff = (x_coordinate - future_x).abs
-      y_diff = (y_coordinate - future_y).abs
-    end
+    x_diff = future_x - x_coordinate
+    y_diff = future_y - y_coordinate
     [x_diff, y_diff]
   end
 
-  def move_front?(x_diff, y_diff)
+  def move_forward?(x_diff, y_diff)
     return true if double_jump_black?(x_diff, y_diff)
     return true if double_jump_white?(x_diff, y_diff)
-    return true if x_diff.zero? && y_diff == 1
+    return true if black_forward?(x_diff, y_diff)
+    return true if white_forward?(x_diff, y_diff)
     false
   end
 
   def double_jump_black?(x_diff, y_diff)
     if color == 'black' && y_coordinate == 6
-      return true if x_diff.zero? && y_diff == 1 || y_diff == 2
+      return true if x_diff.zero? && y_diff == -1 || y_diff == -2
     else
       false
     end
@@ -48,5 +41,13 @@ class Pawn < Piece
     else
       false
     end
+  end
+
+  def black_forward?(x_diff, y_diff)
+    x_diff.zero? && (y_diff == -1 && color == 'black') ? true : false
+  end
+
+  def white_forward?(x_diff, y_diff)
+    x_diff.zero? && (y_diff == 1 && color == 'white') ? true : false
   end
 end
