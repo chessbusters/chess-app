ee02201d85c37ea72faeb4c9121db93355933f62
Joe Spedale <jspedale@gmail.com>
2016-08-25 22:49:45 +0000

    Added some methods to bishop.rb

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,22 @@
 class Bishop < Piece
+  def valid_move?(future_x, future_y)
+    !obstructed?(future_x, future_y) && valid_bishop_range?(future_x, future_y)
+  end
+
+  def valid_bishop_range?(future_x, future_y)
+    x_diff, y_diff = find_diffs(future_x, future_y)
+    return true if move_diag?(x_diff, y_diff)
+  end
+
+  private
+
+  def find_diffs(future_x, future_y)
+    x_diff = (x_coordinate - future_x).abs
+    y_diff = (y_coordinate - future_y).abs
+    [x_diff, y_diff]
+  end
+
+  def move_diag?(x_diff, y_diff)
+    return true if x_diff == 1 && y_diff == 1
+  end
 end
