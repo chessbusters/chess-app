ca6beabcc093da0179ff614e8a423ca5bb67183a
Joe Spedale <jspedale@gmail.com>
2016-08-26 23:51:24 +0000

    fixed rubocop errors

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,15 +2,13 @@ FactoryGirl.define do
   factory :piece do
   end
 
-<<<<<<< HEAD
   factory :game do
   end
-=======
+
   factory :user do
     name 'chessy'
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
->>>>>>> master
     end
     password 'secretPassword'
     password_confirmation 'secretPassword'
