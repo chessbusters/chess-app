e03145635b1ab375cf5f4d1b72e613a81c1bf615
Antoine <maciaantoine@yahoo.fr>
2016-10-11 08:26:44 +0000

    created chessboard component, display chessboard successfuly

--- app/assets/javascripts/components/chessboard.js.jsx
+++ app/assets/javascripts/components/chessboard.js.jsx
@@ -1,6 +1,31 @@
-class Chessboard extends React.Component {
-  render () {
-    return <div />;
-  }
-}
+var Chessboard = React.createClass({
+
+  renderMeow: function(){
+    var array = [1,2,3,4,5,6,7,8];
+    return (
+      <table>
+          {array.map(function(row, rowindex){
+            return <tr key={rowindex}>
+              {array.map(function(col, colindex){
+             return <td className='{col}x#{row}' key={colindex}/>
+        })
+      }
+          </tr>
+        })
+    }
+    </table>
+    )
+  },
+
+
+  render: function() {
+    return(
+      <div>
+        <h1> your game</h1>
+<br />
+<h2> this.props.name </h2>
+        {this.renderMeow()}
+    </div>
+  );}
+});
 
